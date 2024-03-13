f1b6b952fc8790b2ed4d922ec371c55cbabdb90a
Matthieu Bleichner <matthieu.bleichner@ifeelsmart.com>
2024-03-13 11:08:49 +0100

    doc(Readme): add readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# Scriptor Artis Front
+
+Frontend part of scriptor artis
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn dev`
+
+Runs the server in the development mode.\
+Open [http://localhost:4000](http://localhost:4000) to view it in the browser.
+
+
+### `secret token`
+
+Ask your admin for scecret token to set in your env file
+
--- prisma/migrations/20240313091913_init/migration.sql
+++ prisma/migrations/20240313091913_init/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
     "firstName" TEXT,
     "lastName" TEXT
 );
@@ -11,8 +12,17 @@ CREATE TABLE "Task" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "title" TEXT NOT NULL,
     "description" TEXT,
-    "state" TEXT NOT NULL,
-    "ownerId" INTEGER
+    "stateId" INTEGER NOT NULL,
+    "ownerId" INTEGER,
+    "priority" TEXT NOT NULL,
+    "date" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "State" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL,
+    "index" INTEGER NOT NULL
 );
 
 -- CreateIndex
